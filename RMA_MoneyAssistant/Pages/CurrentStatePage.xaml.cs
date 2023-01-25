838accd9aafea29eddb3af1a2dc3702a897aed02
Belma Đelilović <belma.djelilovic.20@size.ba>
2023-01-25 22:46:59 +0100

    primijenjen drugi tab

---
+++ RMA_MoneyAssistant/Pages/CurrentStatePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace RMA_MoneyAssistant.Pages;
+
+public partial class CurrentStatePage : ContentPage
+{
+	public CurrentStatePage()
+	{
+		InitializeComponent();
+	}
+}
--- RMA_MoneyAssistant/Pages/FirstTab.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace RMA_MoneyAssistant.Pages;
-
-public partial class FirstTab : ContentPage
-{
-	public FirstTab()
-	{
-		InitializeComponent();
-	}
-}
