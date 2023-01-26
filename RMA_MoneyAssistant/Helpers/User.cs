d9ea0f41e9146b9e49fdb7fbea24b1f53047c3fd
Naadii <nadinakolic@gmail.com>
2023-01-26 01:40:32 +0100

    profilePage

--- RMA_MoneyAssistant/Helpers/User.cs
+++ RMA_MoneyAssistant/Helpers/User.cs
@@ -10,5 +10,7 @@ namespace RMA_MoneyAssistant.Helpers
     {
         public string UserName { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
